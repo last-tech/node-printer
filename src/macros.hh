eda092d09ca756c86713fdff82b232bff0423594
Sergi Avila <sergiavilafernandez@gmail.com>
2026-07-03 11:49:38 +0200

    chore: remove legacy pre-Node 22 compatibility code
    The package requires Node >= 22, so the old preprocessor branches are
    dead code. Collapse them to their modern form:
    - macros.hh: drop NODE_VERSION_AT_LEAST(0,11,10)/(4,0,0)/(12,0,0) and
      NODE_MODULE_VERSION>73 fallbacks; keep the current-V8 definitions.
    - node_printer.cc: drop the NODE_MAJOR_VERSION>=10 guard around
      NAN_MODULE_WORKER_ENABLED.
    - Remove now-unused <node_version.h> includes.
    
    Verified the POSIX build compiles locally (node-gyp rebuild, Node 24);
    Windows is covered by the prebuild CI.

--- src/macros.hh
+++ src/macros.hh
@@ -2,48 +2,23 @@
 #define NODE_PRINTER_SRC_MACROS_H
 
 #include <nan.h>
-#include <node_version.h>
 
-// NODE_MODULE_VERSION was incremented for v0.11
+#define MY_NODE_MODULE_ISOLATE_DECL v8::Isolate* isolate = v8::Isolate::GetCurrent();
+#define MY_NODE_MODULE_ISOLATE      isolate
+#define MY_NODE_MODULE_HANDLESCOPE MY_NODE_MODULE_ISOLATE_DECL Nan::HandleScope scope
+#define V8_VALUE_NEW(type, value)   v8::type::New(MY_NODE_MODULE_ISOLATE, value)
+#define V8_VALUE_NEW_DEFAULT(type)   v8::type::New(MY_NODE_MODULE_ISOLATE)
+#define V8_STRING_NEW_UTF8(value)   v8::String::NewFromUtf8(MY_NODE_MODULE_ISOLATE, value).ToLocalChecked()
+#define V8_STRING_NEW_2BYTES(value)   v8::String::NewFromTwoByte(MY_NODE_MODULE_ISOLATE, value).ToLocalChecked()
 
-#if NODE_VERSION_AT_LEAST(0, 11, 10)
-#  define MY_NODE_MODULE_ISOLATE_DECL v8::Isolate* isolate = v8::Isolate::GetCurrent();
-#  define MY_NODE_MODULE_ISOLATE      isolate
-#  define MY_NODE_MODULE_HANDLESCOPE MY_NODE_MODULE_ISOLATE_DECL Nan::HandleScope scope
-#  define V8_VALUE_NEW(type, value)   v8::type::New(MY_NODE_MODULE_ISOLATE, value)
-#  define V8_VALUE_NEW_DEFAULT(type)   v8::type::New(MY_NODE_MODULE_ISOLATE)
-#  if NODE_MODULE_VERSION > 73
-#   define V8_STRING_NEW_UTF8(value)   v8::String::NewFromUtf8(MY_NODE_MODULE_ISOLATE, value).ToLocalChecked()
-#   define V8_STRING_NEW_2BYTES(value)   v8::String::NewFromTwoByte(MY_NODE_MODULE_ISOLATE, value).ToLocalChecked()
-#  else
-#    define V8_STRING_NEW_UTF8(value)   v8::String::NewFromUtf8(MY_NODE_MODULE_ISOLATE, value)
-#    define V8_STRING_NEW_2BYTES(value)   v8::String::NewFromTwoByte(MY_NODE_MODULE_ISOLATE, value)
-#  endif
-
-#  define RETURN_EXCEPTION(msg)  isolate->ThrowException(Nan::Error(msg));    \
+#define RETURN_EXCEPTION(msg)  isolate->ThrowException(Nan::Error(msg));    \
     return
 
-#  define RETURN_EXCEPTION_STR(msg) RETURN_EXCEPTION(V8_STRING_NEW_UTF8(msg))
-#  define MY_NODE_MODULE_RETURN_VALUE(value)   iArgs.GetReturnValue().Set(value);   \
+#define RETURN_EXCEPTION_STR(msg) RETURN_EXCEPTION(V8_STRING_NEW_UTF8(msg))
+#define MY_NODE_MODULE_RETURN_VALUE(value)   iArgs.GetReturnValue().Set(value);   \
     return
-#  define MY_NODE_MODULE_RETURN_UNDEFINED()   return
-#else
-#  define MY_NODE_MODULE_ISOLATE_DECL
-#  define MY_NODE_MODULE_ISOLATE
-#  define MY_NODE_MODULE_HANDLESCOPE Nan::HandleScope scope;
-#  define V8_VALUE_NEW(type, value)   v8::type::New(value)
-#  define V8_VALUE_NEW_DEFAULT(type)   v8::type::New()
-#  define V8_STRING_NEW_UTF8(value)   Nan::Utf8String(value)
-#  define V8_STRING_NEW_2BYTES(value)   v8::String::New(value)
+#define MY_NODE_MODULE_RETURN_UNDEFINED()   return
 
-#  define RETURN_EXCEPTION(msg) return v8::ThrowException(Nan::Error(msg)) 
-
-#  define RETURN_EXCEPTION_STR(msg) RETURN_EXCEPTION(V8_STRING_NEW_UTF8(msg))
-#  define MY_NODE_MODULE_RETURN_VALUE(value)   return scope.Close(value)
-#  define MY_NODE_MODULE_RETURN_UNDEFINED()   return scope.Close(v8::Undefined())
-#endif
-
-#if NODE_VERSION_AT_LEAST(4, 0, 0)
 #define V8_LOCAL_STRING_FROM_VALUE(value) value->ToString(Nan::GetCurrentContext()).FromMaybe(v8::Local<v8::String>())
 #define REQUIRE_ARGUMENT_INTEGER(args, i, var)                             \
     int var;                                                                   \
@@ -53,25 +28,9 @@
     else {                                                                     \
         RETURN_EXCEPTION_STR("Argument " #i " must be an integer");                 \
     }
-#else
-#define V8_LOCAL_STRING_FROM_VALUE(value) value->ToString()
-#define REQUIRE_ARGUMENT_INTEGER(args, i, var)                             \
-    int var;                                                                   \
-    if (args[i]->IsInt32()) {                                             \
-        var = args[i]->Int32Value();                                           \
-    }                                                                          \
-    else {                                                                     \
-        RETURN_EXCEPTION_STR("Argument " #i " must be an integer");                 \
-    }
-#endif
 
-#if NODE_VERSION_AT_LEAST(4, 0, 0)
 #define MY_MODULE_SET_METHOD(exports, name, method) Nan::SetMethod(exports, name, method)
 #define MY_NODE_MODULE_CALLBACK(name) void name(const Nan::FunctionCallbackInfo<v8::Value>& iArgs)
-#else
-#define MY_MODULE_SET_METHOD(exports, name, method) NODE_SET_METHOD(exports, name, method)
-#define MY_NODE_MODULE_CALLBACK(name) void name(const v8::FunctionCallbackInfo<v8::Value>& iArgs)
-#endif
 
 #define V8_STR_CONC(left, right)                              \
 	v8::String::Concat(V8_STRING_NEW_UTF8(left), V8_STRING_NEW_UTF8(right))
@@ -111,15 +70,9 @@
     ARG_CHECK_STRING(args, i);                                                       \
     Nan::Utf8String var(V8_LOCAL_STRING_FROM_VALUE(args[i])); \
 
-#if NODE_VERSION_AT_LEAST(12, 0, 0)
-    #define REQUIRE_ARGUMENT_STRINGW(args, i, var)                                        \
-        ARG_CHECK_STRING(args, i);                                                       \
-        v8::String::Value var(MY_NODE_MODULE_ISOLATE, (args[i]));
-#else
-    #define REQUIRE_ARGUMENT_STRINGW(args, i, var)                                        \
-        ARG_CHECK_STRING(args, i);                                                       \
-        v8::String::Value var(V8_LOCAL_STRING_FROM_VALUE(args[i]));
-#endif
+#define REQUIRE_ARGUMENT_STRINGW(args, i, var)                                        \
+    ARG_CHECK_STRING(args, i);                                                       \
+    v8::String::Value var(MY_NODE_MODULE_ISOLATE, (args[i]));
 
 #define OPTIONAL_ARGUMENT_FUNCTION(i, var)                                     \
     v8::Local<v8::Function> var;                                                       \
